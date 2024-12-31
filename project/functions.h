3e8e01ab43658585a1bf4440590721ca08fd6968
duydoan169 <hanh250470@gmail.com>
2024-12-31 13:05:02 +0700

    Add files via upload

---
+++ project/functions.h
@@ -0,0 +1,8 @@
+#include "dataTypes.h"
+
+void roleMenu();
+void manageMenu();
+void studentMenu();
+void inputStudent(Student student[], int *length);
+void printStudent(Student student[], int length);
+void login();
