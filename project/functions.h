4ff70bf919bff4e8c3ab183dcdf778fc1252a505
duydoan169 <hanh250470@gmail.com>
2024-12-31 13:04:35 +0700

    Delete project directory

--- project/functions.h
+++
@@ -1,8 +0,0 @@
-#include "dataTypes.h"
-
-void roleMenu();
-void manageMenu();
-void studentMenu();
-void inputStudent(Student student[], int *length);
-void printStudent(Student student[], int length);
-void login();
